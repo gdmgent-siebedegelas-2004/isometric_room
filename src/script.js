cfc32c93dd0c2bd87c7e1dc21d276ba714b4d4d3
gdmgent-siebedegelas-2004 <knolpowermegafan@gmail.com>
2024-05-29 23:15:43 +0200

    adjusted js, new glb and bakes - FINISHED THE PROJECT

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 import GUI from 'lil-gui'
 import * as THREE from 'three'
-import { gsap } from 'gsap'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
@@ -16,7 +15,7 @@ const gui = new GUI({
 gui.close();
 gui.hide();
 
-if(window,location.hash === '#debug'){
+if(window.location.hash === '#debug'){
     gui.show()
 }
 
@@ -87,10 +86,12 @@ gltfLoader.setDRACOLoader(dracoLoader)
 /**
  * Textures
  */
-const bakedTexture1 = textureLoader.load('textures/baked_1.jpg');
-const bakedTexture2 = textureLoader.load('textures/baked_2.jpg');
+const bakedTexture1 = textureLoader.load('textures/fullScene.jpg');
+const bakedTexture2 = textureLoader.load('textures/wallAndBars.jpg');
 bakedTexture1.flipY = false;
+bakedTexture2.flipY = false;
 bakedTexture1.colorSpace = THREE.SRGBColorSpace;
+bakedTexture2.colorSpace = THREE.SRGBColorSpace;
 
 /**
  * Materials
@@ -106,30 +107,86 @@ const material2 = new THREE.MeshBasicMaterial({
     map: bakedTexture2
 })
 
+
+// GLTF loader
+let mixer = null;
+let actions = [];
+let model = null;
 gltfLoader.load(
     'models/isometric_room.glb',
-    (gltf) =>
-    {
-        gltf.scene.traverse((child) =>
-        {
-            if(child.isMesh){
-                child.material = material1;
+    (gltf) => {
+        model = gltf.scene;
+        mixer = new THREE.AnimationMixer(model);
 
-                if(child.name === 'Bars') {
+        if (gltf.animations.length > 0) {
+            gltf.animations.forEach((clip) => {
+                const action = mixer.clipAction(clip);
+                actions.push(action);
+                action.play();
+            });
+        }
+
+        model.traverse((child) => {
+            if (child.isMesh) {
+                if (child.name === 'Bars' || child.name === 'Right_Wall') {
                     child.material = material2;
+                } else {
+                    child.material = material1;
                 }
             }
         });
-        
-        const emulator = gltf.scene.children.find(child => child.name === 'Emulator');
+
+        const emulator = model.children.find(child => child.name === 'Emulator');
         if (emulator) {
             emulator.material = material1;
         }
-        
-        scene.add(gltf.scene);
+        // console.log(gltf);
+
+        scene.add(model);
+        addModelToGUI(model);
+        addAnimationControlsToGUI();
+        addZoomControlsToGUI();
     }
 );
 
+const addModelToGUI = (model) => {
+    const folder = gui.addFolder('Model Rotation');
+    folder.add(model.rotation, 'x').min(0).max(1.7).step(0.01).name('Rotation X');
+    folder.add(model.rotation, 'y').min(0).max(0.9).step(0.01).name('Rotation Y');
+    folder.add(model.rotation, 'z').min(0).max(0.5).step(0.01).name('Rotation Z');
+}
+
+// Animation control functions
+const stopAllAnimations = () => {
+    actions.forEach(action => action.stop());
+}
+
+const playAllAnimations = () => {
+    actions.forEach(action => action.play());
+}
+
+const removeAllAnimations = () => {
+    actions.forEach(action => {
+        mixer.uncacheAction(action.getClip(), action.getRoot());
+    });
+    actions = [];
+}
+
+const addAnimationControlsToGUI = () => {
+    const animationFolder = gui.addFolder("Animations");
+    animationFolder.add({ stopAllAnimations }, "stopAllAnimations").name("Stop All Animations");
+    animationFolder.add({ playAllAnimations }, "playAllAnimations").name("Play All Animations");
+    animationFolder.add({ removeAllAnimations }, "removeAllAnimations").name("Remove All Animations");
+    animationFolder.open();
+}
+
+// Zoom control functions
+const addZoomControlsToGUI = () => {
+    const zoomFolder = gui.addFolder("Zoom Controls");
+    zoomFolder.add(controls, "minDistance").min(1).max(20).step(0.1).name("Zoom out");
+    zoomFolder.add(controls, "maxDistance").min(1).max(20).step(0.1).name("Zoom in");
+    zoomFolder.open();
+}
 
 
 /**
@@ -228,59 +285,54 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 
+
 /**
  * Animate
  */
-const raycaster = new THREE.Raycaster()
-
-const clock = new THREE.Clock()
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-
+const raycaster = new THREE.Raycaster();
+const clock = new THREE.Clock();
+const tick = () => {
+    const deltaTime = clock.getDelta();
+    
     // Update controls
-    controls.update()
+    controls.update();
 
-    if(sceneReady){
-        for(const point of points)
-            {
-                const screenPosition = point.position.clone()
-                screenPosition.project(camera)
-    
-                raycaster.setFromCamera(screenPosition, camera)
-                const intersects = raycaster.intersectObjects(scene.children, true)
-    
-                if(intersects.length === 0 && debugObject.poi)
-                    {
-                        point.element.classList.add('visible')
-                    }
-                    else
-                    {
-                        const intersectionDistance = intersects[0].distance
-                        const pointDistance = point.position.distanceTo(camera.position)
-            
-                        if(intersectionDistance < pointDistance)
-                        {
-                            point.element.classList.remove('visible')
-                        }
-                        else if(intersectionDistance > pointDistance && debugObject.poi)
-                        {
-                            point.element.classList.add('visible')
-                        }
-                    }
-        
-                const translateX = screenPosition.x * sizes.width * 0.5
-                const translateY = - screenPosition.y * sizes.height * 0.5
-                point.element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`
+    if (sceneReady) {
+        for (const point of points) {
+            const screenPosition = point.position.clone();
+            screenPosition.project(camera);
+
+            raycaster.setFromCamera(screenPosition, camera);
+            const intersects = raycaster.intersectObjects(scene.children, true);
+
+            if (intersects.length === 0 && debugObject.poi) {
+                point.element.classList.add('visible');
+            } else {
+                const intersectionDistance = intersects[0].distance;
+                const pointDistance = point.position.distanceTo(camera.position);
+
+                if (intersectionDistance < pointDistance) {
+                    point.element.classList.remove('visible');
+                } else if (intersectionDistance > pointDistance && debugObject.poi) {
+                    point.element.classList.add('visible');
+                }
             }
+
+            const translateX = screenPosition.x * sizes.width * 0.5;
+            const translateY = -screenPosition.y * sizes.height * 0.5;
+            point.element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`;
+        }
     }
-    
+
+    if (mixer) {
+        mixer.update(deltaTime);
+    }
 
     // Render
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    window.requestAnimationFrame(tick);
 }
 
-tick()
+tick();
